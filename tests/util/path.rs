5a7dfcd78b557c717052eb229f6b505bdc9d9b3b
Iain Laird <ikl@st-andrews.ac.uk>
2021-08-19 22:21:09 +0100

    Fix test utilities paths
    Actually return the fixtures directory now.

--- tests/util/path.rs
+++ tests/util/path.rs
@@ -5,19 +5,13 @@ pub fn exe_path() -> PathBuf {
     env::current_exe().unwrap()
 }
 
-pub fn root() -> PathBuf {
-    exe_path()
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .join("tests")
+pub fn test_root() -> PathBuf {
+    let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    root.join("tests")
 }
 
 pub fn fixtures() -> PathBuf {
-    root().join("fixtures")
+    test_root().join("fixtures")
 }
 
 pub fn fixture(name: &str) -> PathBuf {
